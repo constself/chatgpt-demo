14d8a0c83cc17e62646ffea620db0aaee766d183
constself <constself@outlook.com>
2023-08-09 11:46:56 +0800

    fix: custom

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,9 +25,9 @@ export default defineConfig({
       registerType: 'autoUpdate',
       injectRegister: 'inline',
       manifest: {
-        name: 'ChatGPT-API Demo',
-        short_name: 'ChatGPT Demo',
-        description: 'A demo repo based on OpenAI API',
+        name: 'PardChat',
+        short_name: 'PardChat',
+        description: 'PardChat based on OpenAI API',
         theme_color: '#212129',
         background_color: '#ffffff',
         icons: [
